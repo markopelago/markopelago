912d72d591b120306a4f202ec102beb4b37d8a1b
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-08-28 11:07:05 +0700

    dashboard goods

--- dashboard_goods.php
+++ dashboard_goods.php
@@ -17,59 +17,41 @@
 		}
 	}
 </script>
-<div class="row scrolling-wrapper">
-	<table class="table table-striped table-hover">
-		<thead> <?=$t->header(["","",v("name"),v("categories"),v("price"),v("condition"),v("stock"),v("unit"),v("description")]);?> </thead>
-		<tbody>
-			<?php
-				$_goods = $db->fetch_all_data("goods",[],"seller_id='".$__seller_id."'","id");
-				if(count($_goods) <= 0){
-					?> <tr class="danger"><td colspan="9" align="center"><b><?=v("data_not_found");?></b></td></tr> <?php
-				} else {
-					foreach($_goods as $goods){
-						$stock_in = $db->fetch_single_data("goods_histories","concat(sum(qty))",["seller_user_id" => $__user_id,"goods_id" => $goods["id"],"in_out" => "in"]);
-						$stock_out = $db->fetch_single_data("goods_histories","concat(sum(qty))",["seller_user_id" => $__user_id,"goods_id" => $goods["id"],"in_out" => "out"]);
-						$stock = $stock_in - $stock_out;
-						$category_ids = pipetoarray($goods["category_ids"]);
-						$categories = "";
-						foreach($category_ids as $category_id){ $categories .= $db->fetch_single_data("categories","name_".$__locale,["id" => $category_id])."; "; }
-						$categories = substr($categories,0,-2);
-						
-						$img_goods_photo = "";
-						$goods_photo = $db->fetch_all_data("goods_photos",[],"goods_id='".$goods["id"]."' ORDER BY seqno")[0];
-						if($goods_photo["id"] > 0){
-							$filename = $goods_photo["filename"];
-							if($filename == "") $filename = "no_goods.png";
-							$img_goods_photo = "<img src='goods/".$filename."' width='80'>";
-						}
-						$onclick = "onclick=\"window.location='goods_view.php?id=".$goods["id"]."';\"";
-						echo $t->row(
-							[
-							 "<span title='".v("view")."' class='glyphicon glyphicon-search btn btn-primary' onclick=\"window.location='goods_view.php?id=".$goods["id"]."'\"></span>".
-							 "&nbsp;<span title='".v("edit")."' class='glyphicon glyphicon-edit btn btn-primary' onclick=\"window.location='goods_edit.php?id=".$goods["id"]."'\"></span>".
-							 "&nbsp;<span title='".v("delete")."' class='glyphicon glyphicon-trash btn btn-warning' onclick=\"delete_goods('".$goods["id"]."');\"></span>",
-							 $img_goods_photo,
-							 $goods["name"],
-							 $categories,
-							 format_amount($goods["price"]),
-							 ($goods["is_new"] == 1)?v("new"):v("second_hand"),
-							 $stock,
-							 $db->fetch_single_data("units","name_".$__locale,["id" => $goods["unit_id"]]),
-							 "<pre style='width:400px;height:150px;'>".$goods["description"]."</pre>"],
-							["class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 $onclick." class='nowrap'",
-							 ],
-							"style='cursor:pointer;'"
-						);
-					}
+<br><br>
+<div class="container">
+	<?php
+		$_goods = $db->fetch_all_data("goods",[],"seller_id='".$__seller_id."'","id");
+		if(count($_goods) <= 0){
+			?> <tr class="danger"><td colspan="2" align="center"><b><?=v("data_not_found");?></b></td></tr> <?php
+		} else {
+			foreach($_goods as $goods){
+				$stock_in = $db->fetch_single_data("goods_histories","concat(sum(qty))",["seller_user_id" => $__user_id,"goods_id" => $goods["id"],"in_out" => "in"]);
+				$stock_out = $db->fetch_single_data("goods_histories","concat(sum(qty))",["seller_user_id" => $__user_id,"goods_id" => $goods["id"],"in_out" => "out"]);
+				$stock = $stock_in - $stock_out;
+				$category_ids = pipetoarray($goods["category_ids"]);
+				$categories = "";
+				foreach($category_ids as $category_id){ $categories .= $db->fetch_single_data("categories","name_".$__locale,["id" => $category_id]).", "; }
+				$categories = substr($categories,0,-2);
+				
+				$img_goods_photo = "";
+				$goods_photo = $db->fetch_all_data("goods_photos",[],"goods_id='".$goods["id"]."' ORDER BY seqno")[0];
+				if($goods_photo["id"] > 0){
+					$filename = $goods_photo["filename"];
+					if($filename == "") $filename = "no_goods.png";
+					$img_goods_photo = "<img src='goods/".$filename."' style=\"width:200px;\">";
 				}
-			?>
-		</tbody>
-	</table>
+				?>
+					<div class="row well">
+						<?="<center><b>".$goods["name"]."</b> (".(($goods["is_new"] == 1)?v("new"):v("second_hand")).")<br><br>".
+							"".$img_goods_photo."</center><br>".
+							"<b>Rp. ".format_amount($goods["price"])."</b><br>".
+							$categories."<br>".
+							v("stock")." : ".$stock." ".$db->fetch_single_data("units","name_".$__locale,["id" => $goods["unit_id"]])."<br>".
+							"<pre class='hidden-xs' style='width:100%;height:150px;'>".$goods["description"]."</pre>".
+							"<span style='width:100%;' title='".v("view")."' class='glyphicon glyphicon-search btn btn-primary' onclick=\"window.location='goods_view.php?id=".$goods["id"]."'\"></span>";?>
+					</div>
+				<?php
+			}
+		}
+	?>
 </div>
