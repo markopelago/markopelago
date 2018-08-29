9bb2427bb529e3324279d5f18dcbae6da7592e14
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-08-30 05:43:08 +0700

    product info

--- category_detail.php
+++ category_detail.php
@@ -14,6 +14,8 @@
 		foreach($products as $product){
 			$img = $db->fetch_single_data("goods_photos","filename",["goods_id"=>$product["id"]],["seqno"]);
 			if($img == "") $img = "no_goods.png";
+			$seller_user_id = $db->fetch_single_data("sellers","user_id",["id"=> $product["seller_id"]]);
+			$seller_location_id = $db->fetch_single_data("user_addresses","location_id",["user_id" => $seller_user_id,"default_seller" => 1]);
 	?>
 		<div class="img-thumbnail goods-thumbnail goods-thumbnail2">
 			<a href="product_detail.php?id=<?=$product["id"];?>">
@@ -22,8 +24,8 @@
 				<div class="price"><p>Rp. <?=format_amount($product["price"]);?> / <?=$db->fetch_single_data("units","name_".$__locale,["id" => $product["unit_id"]]);?></p></div>
 				<button class="btn btn-primary btn-sm" style="width:100%"><?=v("buy");?></button>
 				<div class="seller-info">
-					<img src="icons/location.png" alt="#" style="width:15px; height:15px;">
-					<?=$db->fetch_single_data("sellers","name",["id"=>$product["seller_id"]]);?>
+					<?=$db->fetch_single_data("sellers","name",["id"=>$product["seller_id"]]);?><br>
+					<span class="glyphicon glyphicon-map-marker"></span> <?=get_location($seller_location_id)[0]["name"];?>
 				</div>
 			</a>
 		</div>
