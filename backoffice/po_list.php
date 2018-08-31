c3378b61489858d38b409759eb7ffce419fd05b3
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-08-31 20:08:44 +0700

    backoffice po

--- backoffice/po_list.php
+++ backoffice/po_list.php
@@ -1,46 +1,80 @@
 <?php include_once "head.php";?>
-<div class="bo_title">PO</div>
-
-<?php
-	$carts = $db->fetch_all_data("transactions",[],"status >= 3 GROUP BY po_no");
+<?php 
+	if($_GET["seller_paid"] == "1"){
+		$db->addtable("transactions");  
+		$db->where("po_no",$_GET["po_no"]);
+		$db->addfield("seller_paid");	$db->addvalue(1);
+		$db->addfield("seller_paid_at");$db->addvalue($__now);
+		$db->addfield("seller_paid_by");$db->addvalue($__username);
+		$updating = $db->update();
+		if($updating["affected_rows"] > 0){
+			$_SESSION["message"] = "Status berhasil diubah";
+			?><script> window.location='po_list.php'; </script><?php
+			exit();
+		} else {
+			$_SESSION["errormessage"] = "Status gagal diubah";
+		}
+	}
+	$carts = $db->fetch_all_data("transactions",[],"status >= 3 GROUP BY po_no"); 
 ?>
+
+<script>
+	function seller_paid(po_no){
+		if(confirm("Anda yakin sudah membayar ke seller yang bersangkutan?")){
+			window.location="?po_no="+po_no+"&seller_paid=1";
+		}
+	}
+</script>
+<div class="bo_title">PO</div>
 	<?=$t->start("","data_content");?>
 	<?=$t->header(["No",
 					"PO No",
-					"Invoice No",
 					"Seller",
 					"Buyer",
 					"Total",
+					"commission",
+					"Payment to Seller",
+					"Seller Bank",
 					"<div onclick=\"sorting('status');\">Status</div>",
 					"<div onclick=\"sorting('transaction_at');\">Transaction At</div>",
 					""]);?>
 	<?php 
 		foreach($carts as $no => $cart){
-			$invoice_no = "";
+			$totgross = 0;
 			$total = 0;
-			$transactions = $db->fetch_all_data("transactions",[],"po_no = '".$cart["po_no"]."'");
-			foreach($transactions as $transaction){
-				if(!$invoices[$transaction["invoice_no"]]){
-					$invoice_no .= $transaction["invoice_no"]."<br>"; 
-					$invoices[$transaction["invoice_no"]] = 1;
-				}
+			$transaction = $db->fetch_all_data("transactions",[],"po_no = '".$cart["po_no"]."'")[0];
+			$transaction_details = $db->fetch_all_data("transaction_details",[],"transaction_id IN (SELECT id FROM transactions WHERE po_no = '".$cart["po_no"]."')");
+			foreach($transaction_details as $transaction_detail){
+				$totgross += ($transaction_detail["gross"] * $transaction_detail["qty"]);
+				$total += $transaction_detail["total"];
 			}
-			$invoice_no = substr($invoice_no,0,-4);
+			$commission = $total - $totgross;
 			$transaction_payments = $db->fetch_all_data("transaction_payments",[],"cart_group = '".$cart["cart_group"]."'")[0];
-			$seller = $db->fetch_single_data("a_users","name",["id" => $transaction["seller_user_id"]]);
+			$seller = $db->fetch_single_data("sellers","name",["user_id" => $transaction["seller_user_id"]]);
+			$seller_bank = $db->fetch_single_data("user_banks","concat('a/n: ',name,'<br>',(SELECT name FROM banks WHERE id=bank_id),' : ',account_no)",["user_id" => $transaction["seller_user_id"],"default_seller" => 1]);
 			$buyer = $db->fetch_single_data("a_users","name",["id" => $transaction["buyer_user_id"]]);
+		
+			if($transaction["status"] < 7){
+				$status = "<b style='color:red;'>Transaction Not Yet Done</b>";
+			}else if($transaction["seller_paid"] == "0"){
+				$status = "Unpaid<br>".$f->input("paid","Paid Now","type='button' onclick=\"seller_paid('".$cart["po_no"]."');\"");
+			} else {
+				$status = "<b style='color:blue;'>Seller Paid At ".$transaction["seller_paid_at"]."</b>";
+			}
 			$actions = 	"<a href=\"invoice_view.php?cart_group=".$cart["cart_group"]."\">View</a>";
 			echo $t->row(
 				[$no+$start+1,
 				$cart["po_no"],
-				$invoice_no,
 				$seller,
 				$buyer,
-				format_amount($transaction_payments["total"]),
+				format_amount($total),
+				format_amount($commission),
+				"<b>".format_amount($totgross)."</b>",
+				$seller_bank,
 				$status,
 				$transaction["transaction_at"],
 				$actions],
-				["align='right' valign='top'","","","align='right' valign='top'","align='right' valign='top'","align='right' valign='top'"]
+				["align='right' valign='top'","","","","align='right' valign='top'","align='right' valign='top'","align='right' valign='top'",""]
 			);
 		} 
 	?>
