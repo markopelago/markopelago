c348f9f1fbf05f176d504528bb2c49bae74dd5bb
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-08-31 18:43:34 +0700

    mypo

--- backoffice/invoice_view.php
+++ backoffice/invoice_view.php
@@ -46,101 +46,94 @@
 		}
 	}
 </script>
-<div style="height:20px;"></div>
 <div class="container">
-	<div class="row sub-title-area" style="border-bottom: 1px solid #ccc;">
-		<div class="sub-title-text">
-            <span class="glyphicon glyphicon-list-alt" style="color:#800000;"></span> &nbsp;Invoice
-		</div>
-	</div>
     <div class="row">
-        <div style="height:20px;"></div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <?php 
-				    foreach($_trxBySeller as $seller_user_id => $transactions){
-				        $seller = $db->fetch_all_data("sellers",[],"user_id = '".$seller_user_id."'")[0];
-                ?>
-                    <table border="1" width="100%">
-                        <tr>
-                            <td colspan="6">Seller : <b><?=$seller["name"];?></b></td>
-                        </tr> 
-						<?php 
-							foreach($transactions as $transaction){
-								$transaction_details = $db->fetch_all_data("transaction_details",[],"id = '".$transaction["id"]."'")[0];
-								$goods  = $db->fetch_all_data("goods",[],"id = '".$transaction_details["goods_id"]."'")[0];
-								$goods_photos  = $db->fetch_all_data("goods_photos",[],"goods_id = '".$goods["id"]."'","seqno")[0];
-								$units  = $db->fetch_all_data("units",[],"id = '".$transaction_details["unit_id"]."'")[0];
-								$transaction_forwarder = $db->fetch_all_data("transaction_forwarder",[],"transaction_id = '".$transaction["id"]."'")[0];
-								
-								$total = $transaction_details["total"] + $transaction_forwarder["total"];
-								$total_tagihan += $total;
+		<?php 
+			foreach($_trxBySeller as $seller_user_id => $transactions){
+				$seller = $db->fetch_all_data("sellers",[],"user_id = '".$seller_user_id."'")[0];
+				$seller_locations = get_location($db->fetch_single_data("user_addresses","location_id",["user_id" => $seller_user_id,"default_seller" => 1]));
+		?>
+			<table border="1" width="100%">
+				<tr>
+					<td colspan="6">
+						<b><?=$seller["name"];?></b><br>
+						<?=$seller_locations[2]["name"];?>, <?=$seller_locations[1]["name"];?>, <?=$seller_locations[0]["name"];?><br>
+					</td>
+				</tr> 
+				<?php 
+					foreach($transactions as $transaction){
+						$transaction_details = $db->fetch_all_data("transaction_details",[],"id = '".$transaction["id"]."'")[0];
+						$goods  = $db->fetch_all_data("goods",[],"id = '".$transaction_details["goods_id"]."'")[0];
+						$goods_photos  = $db->fetch_all_data("goods_photos",[],"goods_id = '".$goods["id"]."'","seqno")[0];
+						$units  = $db->fetch_all_data("units",[],"id = '".$transaction_details["unit_id"]."'")[0];
+						$transaction_forwarder = $db->fetch_all_data("transaction_forwarder",[],"transaction_id = '".$transaction["id"]."'")[0];
+						
+						$total = $transaction_details["total"] + $transaction_forwarder["total"];
+						$total_tagihan += $total;
+				?>
+				<tr>
+					<td colspan="4">
+						<div class="col-md-2">
+							<img src="../goods/<?=$goods_photos["filename"]?>" style="width:120px;"> 
+						</div>
+						<div class="col-md-10">
+							<b><?=$goods["name"]?></b><br>
+							<?=$transaction_details["qty"]?> <?=$units["name_".$__locale]?> x Rp <?=format_amount($transaction_details["price"])?><br>
+							Weight per Unit : <?=($goods["weight"]/1000);?> Kg
+						</div>
+					</td>
+					<td colspan="2" align="right">
+						Sub Total<br>
+						Rp. <?=format_amount($transaction_details["total"])?>
+					</td>   
+				</tr>
+				<tr>
+					<td colspan="5">
+						<u>Delivery Destination :</u><br><br>
+						<b><?=$transaction_forwarder["user_address_pic"];?></b> <br>
+						<?=$transaction_forwarder["user_address"];?> <br>
+						<?php
+							$locations = get_location($transaction_forwarder["user_address_location_id"]);
+							echo $locations[3]["name"].", ".$locations[2]["name"]."<br>";
+							echo $locations[1]["name"].", ".$locations[0]["name"],", ".$locations[3]["zipcode"]."<br>";
 						?>
-                        <tr>
-                            <td colspan="4">
-                                <div class="col-md-2">
-                                    <img src="../goods/<?=$goods_photos["filename"]?>" style="width:120px;"> 
-                                </div>
-                                <div class="col-md-10">
-                                    <b><?=$goods["name"]?></b><br>
-                                    <?=$transaction_details["qty"]?> <?=$units["name_".$__locale]?> x Rp <?=format_amount($transaction_details["price"])?><br>
-									Weight per Unit : <?=($goods["weight"]/1000);?> Kg
-                                </div>
-                            </td>
-                            <td colspan="2" align="right">
-								Sub Total<br>
-                                Rp. <?=format_amount($transaction_details["total"])?>
-                            </td>   
-                        </tr>
-                        <tr>
-                            <td colspan="5">
-								<u>Delivery Destination :</u><br><br>
-								<b><?=$transaction_forwarder["user_address_pic"];?></b> <br>
-								<?=$transaction_forwarder["user_address"];?> <br>
-								<?php
-									$locations = get_location($transaction_forwarder["user_address_location_id"]);
-									echo $locations[3]["name"].", ".$locations[2]["name"]."<br>";
-									echo $locations[1]["name"].", ".$locations[0]["name"],", ".$locations[3]["zipcode"]."<br>";
-								?>
-								<?=$transaction_forwarder["user_address_phone"];?>                                    
-                            </td>
-						</tr>
-						<tr>
-							<td colspan="3" width="70%"> 
-								<u>Courier Service :</u><br> <?=$transaction_forwarder["name"];?> -- <?=explode(" (",$transaction_forwarder["courier_service"])[0];?>
-							</td>
-                            <td nowrap width="15%" align="right">
-								Weight<br> <?=($transaction_forwarder["weight"]*$transaction_forwarder["qty"]/1000);?> Kg
-							</td> 
-                            <td nowrap width="15%" align="right">
-								Shipping Charges<br> Rp <?=format_amount($transaction_forwarder["total"])?>
-							</td>
-                        </tr>
-                        <tr>
-                            <td colspan="6" align="right"><b> Total : Rp <?=format_amount($total)?></b></td>
-                        </tr>
-                        <tr><td colspan="6"></td></tr>
-						<?php } ?>
-                    </table>
-					<br>
+						<?=$transaction_forwarder["user_address_phone"];?>                                    
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" width="70%"> 
+						<u>Courier Service :</u><br> <?=$transaction_forwarder["name"];?> -- <?=explode(" (",$transaction_forwarder["courier_service"])[0];?>
+					</td>
+					<td nowrap width="15%" align="right">
+						Weight<br> <?=($transaction_forwarder["weight"]*$transaction_forwarder["qty"]/1000);?> Kg
+					</td> 
+					<td nowrap width="15%" align="right">
+						Shipping Charges<br> Rp <?=format_amount($transaction_forwarder["total"])?>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="6" align="right"><b> Total : Rp <?=format_amount($total)?></b></td>
+				</tr>
+				<tr><td colspan="6"></td></tr>
 				<?php } ?>
-				<table width="100%">
-					<tr>
-						<td align="right"><b>Total Bill : &nbsp;&nbsp;Rp. <?=format_amount($total_tagihan)?></b></td>
-					</tr>
-				</table>
-            </div>
-        </div>
-			<h3><b>Payment Info:</b></h3>
-			<table>
-				<tr><td valign="top"><b>Transaction At</b></td><td valign="top"><b>:</b></td><td><?=format_tanggal($transactions[0]["transaction_at"]);?></td></tr>
-				<tr><td valign="top"><b>Transfer At</b></td><td valign="top"><b>:</b></td><td><?=format_tanggal($transaction_payments["transfer_at"]);?></td></tr>
-				<tr><td valign="top"><b>Uniq Code</b></td><td valign="top"><b>:</b></td><td align="right"><?=format_amount($transaction_payments["uniqcode"]);?></td></tr>
-				<tr><td valign="top"><b>Total Amount</b></td><td valign="top"><b>:</b></td><td align="right"><b><?=format_amount($transaction_payments["total"]);?></b></td></tr>
-				<tr><td valign="top"><b>Bank From</b></td><td valign="top"><b>:</b></td><td><?=$bank_from;?></td></tr>
-				<tr><td valign="top"><b>Bank To</b></td><td valign="top"><b>:</b></td><td><?=$bank_to;?></td></tr>
-				<tr><td colspan="3"><?=$status;?></td></tr>
 			</table>
+			<br>
+		<?php } ?>
+		<table width="100%">
+			<tr>
+				<td align="right"><b>Total Bill : &nbsp;&nbsp;Rp. <?=format_amount($total_tagihan)?></b></td>
+			</tr>
+		</table>
+		<h3><b>Payment Info:</b></h3>
+		<table>
+			<tr><td valign="top"><b>Transaction At</b></td><td valign="top"><b>:</b></td><td><?=format_tanggal($transactions[0]["transaction_at"]);?></td></tr>
+			<tr><td valign="top"><b>Transfer At</b></td><td valign="top"><b>:</b></td><td><?=format_tanggal($transaction_payments["transfer_at"]);?></td></tr>
+			<tr><td valign="top"><b>Uniq Code</b></td><td valign="top"><b>:</b></td><td align="right"><?=format_amount($transaction_payments["uniqcode"]);?></td></tr>
+			<tr><td valign="top"><b>Total Amount</b></td><td valign="top"><b>:</b></td><td align="right"><b><?=format_amount($transaction_payments["total"]);?></b></td></tr>
+			<tr><td valign="top"><b>Bank From</b></td><td valign="top"><b>:</b></td><td><?=$bank_from;?></td></tr>
+			<tr><td valign="top"><b>Bank To</b></td><td valign="top"><b>:</b></td><td><?=$bank_to;?></td></tr>
+			<tr><td colspan="3"><?=$status;?></td></tr>
+		</table>
 		<br>
 		<?php if($db->fetch_single_data("transactions","id",["cart_group" => $cart_group,"status" => "2:<="]) <= 0){ ?>
 			<span style="width:100%;color:green;"><h3><b>PAID</b></h3></span>
--- mypo.php
+++ mypo.php
@@ -79,88 +79,84 @@
 		}
 	}
 </script>
-<div style="height:20px;"></div>
+
 <div class="container">
-	<div class="row sub-title-area" style="border-bottom: 1px solid #ccc;">
-		<div class="sub-title-text">
-            <span class="glyphicon glyphicon-list-alt" style="color:#800000;"></span> &nbsp;Purchase Order
-		</div>
-	</div>
     <div class="row">
-        <div style="height:20px;"></div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-				<table class="table table-bordered" width="100%">
-					<?php 
-						foreach($transactions as $transaction){
-							$transaction_details = $db->fetch_all_data("transaction_details",[],"id = '".$transaction["id"]."'")[0];
-							$goods  = $db->fetch_all_data("goods",[],"id = '".$transaction_details["goods_id"]."'")[0];
-							$goods_photos  = $db->fetch_all_data("goods_photos",[],"goods_id = '".$goods["id"]."'","seqno")[0];
-							$units  = $db->fetch_all_data("units",[],"id = '".$transaction_details["unit_id"]."'")[0];
-							$transaction_forwarder = $db->fetch_all_data("transaction_forwarder",[],"transaction_id = '".$transaction["id"]."'")[0];
-							
-							$total = $transaction_details["total"] + $transaction_forwarder["total"];
-							$total_tagihan += $total;
+		<h4 class="well"><b><span class="glyphicon glyphicon-list-alt" style="color:#800000;"></span> &nbsp;Purchase Order</b></h4>
+	</div>
+</div>
+<div class="container">
+    <div class="row">
+		<table class="table table-bordered" width="100%">
+			<?php 
+				foreach($transactions as $transaction){
+					$transaction_details = $db->fetch_all_data("transaction_details",[],"id = '".$transaction["id"]."'")[0];
+					$goods  = $db->fetch_all_data("goods",[],"id = '".$transaction_details["goods_id"]."'")[0];
+					$goods_photos  = $db->fetch_all_data("goods_photos",[],"goods_id = '".$goods["id"]."'","seqno")[0];
+					$units  = $db->fetch_all_data("units",[],"id = '".$transaction_details["unit_id"]."'")[0];
+					$transaction_forwarder = $db->fetch_all_data("transaction_forwarder",[],"transaction_id = '".$transaction["id"]."'")[0];
+					
+					$total = $transaction_details["total"] + $transaction_forwarder["total"];
+					$total_tagihan += $total;
+			?>
+			<tr>
+				<td colspan="4">
+					<div class="col-md-2">
+						<img src="goods/<?=$goods_photos["filename"]?>" style="width:120px;"> 
+					</div>
+					<div class="col-md-10">
+						<b><?=$goods["name"]?></b><br>
+						<?=$transaction_details["qty"]?> <?=$units["name_".$__locale]?> x Rp <?=format_amount($transaction_details["price"])?><br>
+						<?=v("weight_per_unit");?> : <?=($goods["weight"]/1000);?> Kg
+					</div>
+				</td>
+				<td colspan="2" align="right" nowrap>
+					Sub Total<br>
+					Rp. <?=format_amount($transaction_details["total"])?>
+				</td>   
+			</tr>
+			<tr>
+				<td colspan="5">
+					<u><?=v("delivery_destination");?> :</u><br><br>
+					<b><?=$transaction_forwarder["user_address_pic"];?></b> <br>
+					<?=$transaction_forwarder["user_address"];?> <br>
+					<?php
+						$locations = get_location($transaction_forwarder["user_address_location_id"]);
+						echo $locations[3]["name"].", ".$locations[2]["name"]."<br>";
+						echo $locations[1]["name"].", ".$locations[0]["name"],", ".$locations[3]["zipcode"]."<br>";
 					?>
-					<tr>
-						<td colspan="4">
-							<div class="col-md-2">
-								<img src="goods/<?=$goods_photos["filename"]?>" style="width:120px;"> 
-							</div>
-							<div class="col-md-10">
-								<b><?=$goods["name"]?></b><br>
-								<?=$transaction_details["qty"]?> <?=$units["name_".$__locale]?> x Rp <?=format_amount($transaction_details["price"])?><br>
-								<?=v("weight_per_unit");?> : <?=($goods["weight"]/1000);?> Kg
-							</div>
-						</td>
-						<td colspan="2" align="right">
-							Sub Total<br>
-							Rp. <?=format_amount($transaction_details["total"])?>
-						</td>   
-					</tr>
-					<tr>
-						<td colspan="5">
-							<u><?=v("delivery_destination");?> :</u><br><br>
-							<b><?=$transaction_forwarder["user_address_pic"];?></b> <br>
-							<?=$transaction_forwarder["user_address"];?> <br>
-							<?php
-								$locations = get_location($transaction_forwarder["user_address_location_id"]);
-								echo $locations[3]["name"].", ".$locations[2]["name"]."<br>";
-								echo $locations[1]["name"].", ".$locations[0]["name"],", ".$locations[3]["zipcode"]."<br>";
-							?>
-							<?=$transaction_forwarder["user_address_phone"];?>                                    
-						</td>
-					</tr>
-					<tr>
-						<td colspan="3" width="70%"> 
-							<u><?=v("courier_service");?> :</u><br> <?=$transaction_forwarder["name"];?> -- <?=explode(" (",$transaction_forwarder["courier_service"])[0];?>
-							<?php
-								if($status >= "5" && $transaction_forwarder["receipt_at"] != "0000-00-00 00:00:00") {
-									echo "<br><b>".v("delivered_at").": ".format_tanggal($transaction_forwarder["receipt_at"]);
-									echo "<br>".v("shipping_receipt_number").": ".$transaction_forwarder["receipt_no"]."</b>";
-								}
-							?>
-						</td>
-						<td nowrap width="15%" align="right">
-							<?=v("weight");?><br> <?=($transaction_forwarder["weight"]*$transaction_forwarder["qty"]/1000);?> Kg
-						</td> 
-						<td nowrap width="15%" align="right">
-							<?=v("shipping_charges");?><br> Rp <?=format_amount($transaction_forwarder["total"])?>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="6" align="right"><b> Total : Rp <?=format_amount($total)?></b></td>
-					</tr>
-					<tr><td colspan="6"></td></tr>
-					<?php } ?>
-				</table>
-				<table width="100%">
-					<tr>
-						<td align="right"><b><?=v("total_bill");?> : &nbsp;&nbsp;Rp. <?=format_amount($total_tagihan)?></b></td>
-					</tr>
-				</table>
-            </div>
-        </div>
+					<?=$transaction_forwarder["user_address_phone"];?>                                    
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" width="70%"> 
+					<u><?=v("courier_service");?> :</u><br> <?=$transaction_forwarder["name"];?> -- <?=explode(" (",$transaction_forwarder["courier_service"])[0];?>
+					<?php
+						if($status >= "5" && $transaction_forwarder["receipt_at"] != "0000-00-00 00:00:00") {
+							echo "<br><b>".v("delivered_at").": ".format_tanggal($transaction_forwarder["receipt_at"]);
+							echo "<br>".v("shipping_receipt_number").": ".$transaction_forwarder["receipt_no"]."</b>";
+						}
+					?>
+				</td>
+				<td nowrap width="15%" align="right">
+					<?=v("weight");?><br> <?=($transaction_forwarder["weight"]*$transaction_forwarder["qty"]/1000);?> Kg
+				</td> 
+				<td nowrap width="15%" align="right">
+					<?=v("shipping_charges");?><br> Rp <?=format_amount($transaction_forwarder["total"])?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="6" align="right"><b> Total : Rp <?=format_amount($total)?></b></td>
+			</tr>
+			<tr><td colspan="6"></td></tr>
+			<?php } ?>
+		</table>
+		<table width="100%">
+			<tr>
+				<td align="right"><h4><b><?=v("total_bill");?> : &nbsp;&nbsp;Rp. <?=format_amount($total_tagihan)?></b></h4></td>
+			</tr>
+		</table>
+		<br>
 		<?php 
 			if($status == "3")	echo $f->input("process_po",v("process_this_po"),"style=\"width:100%;\" onclick=\"changeStatus(4);\"","btn btn-success");
 			if($status == "4")	echo $f->input("po_delivered",v("po_was_delivered"),"style=\"width:100%;\" onclick=\"changeStatus(5);\"","btn btn-success");
