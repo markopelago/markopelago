3f9f54b6a85b9953857b9c456b81da2d202cf364
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-09-11 06:16:53 +0700

    db altered on server

--- alter_db/alter_db.sql
+++ alter_db/alter_db.sql
@@ -1,22 +1,3 @@
-DROP TABLE IF EXISTS `mail_queue`;
-CREATE TABLE `mail_queue` (
-  `id` int(11) NOT NULL,
-  `subject` text NOT NULL,
-  `address` text NOT NULL,
-  `body` text NOT NULL,
-  `replyto` text NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL,
-  `created_by` varchar(100) NOT NULL,
-  `created_ip` varchar(20) NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `updated_by` varchar(100) NOT NULL,
-  `updated_ip` varchar(20) NOT NULL,
-  `xtimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-ALTER TABLE `mail_queue` ADD PRIMARY KEY (`id`), ADD KEY `status` (`status`);
-ALTER TABLE `mail_queue` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 rfo
 	id
 	user_id
