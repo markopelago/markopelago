1ef8dea5614ae26bd8cee99870e65142bc39c55c
Warih Hadi Suryono <warih_suryono@yahoo.com>
2018-10-30 19:58:48 +0700

    db altered on server

--- alter_db/alter_db.sql
+++ alter_db/alter_db.sql
@@ -1,9 +1,3 @@
-ALTER TABLE `a_users` ADD `marko_id` VARCHAR(100) NOT NULL AFTER `group_id`;
-UPDATE a_users SET marko_id=concat(replace(name,' ','_'),'_',id);
-ALTER TABLE `a_users` ADD UNIQUE(`marko_id`);
-
-
-
 rfo
 	id
 	user_id
