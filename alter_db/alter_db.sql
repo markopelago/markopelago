ca885cda0996e91fbd35ad42471ed8aa96265d11
Warih Hadi Suryono <warih_suryono@yahoo.com>
2019-01-19 21:50:21 +0700

    db altered on server

--- alter_db/alter_db.sql
+++ alter_db/alter_db.sql
@@ -1,7 +1,3 @@
-ALTER TABLE `transaction_details` ADD `is_reviewed` SMALLINT NOT NULL AFTER `notes`, ADD `review_at` DATETIME NOT NULL AFTER `is_reviewed`, ADD `review_level` INT NOT NULL AFTER `review_at`, ADD `review_description` TEXT NOT NULL AFTER `review_level`, ADD INDEX (`is_reviewed`);
-ALTER TABLE `transaction_details` ADD `review_id_read` SMALLINT NOT NULL AFTER `review_description`, ADD `review_read_at` DATETIME NOT NULL AFTER `review_id_read`, ADD INDEX (`review_id_read`);
-
-
 rfo
 	id
 	user_id
