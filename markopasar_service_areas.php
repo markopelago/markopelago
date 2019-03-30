27dbb5b03a6bb6bcc36f365525d5f4cbf0ebcdd9
Rigen <rigenwildanu@gmail.com>
2019-03-30 22:57:23 +0700

    marko pasar service areas done (FINAL)

--- markopasar_service_areas.php
+++ markopasar_service_areas.php
@@ -27,6 +27,6 @@
 		</tr>
 	</table>
 </div>
-<div style="height:40px;padding-bottom:60px;text-align: center;"><h1>Nantikan layanan di kota kota lainnya</h1></div>
+<div style="height:40px;padding-bottom:<?=(!isMobile())?"60px":"25%";?>;text-align: center;"><h1>Nantikan layanan di kota kota lainnya</h1></div>
 <?php include_once "categories_footer.php"; ?>
 <?php include_once "footer.php"; ?>
