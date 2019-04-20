3c6686d9cc31912d0356102a96eaca6a440d1380
Rigen <rigenwildanu@gmail.com>
2019-04-21 01:57:23 +0700

    ganti banner header (final)

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 	<div class="carousel-inner">
 		<div class="item active">	<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>9.png" style="object-fit:cover;width:100%;"></div>
-		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>6.png" style="object-fit:cover;width:100%;"></div>
-		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>7.png" style="object-fit:cover;width:100%;"></div>
-		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>8.png" style="object-fit:cover;width:100%;"></div>
+		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>10.png" style="object-fit:cover;width:100%;"></div>
+		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>11.png" style="object-fit:cover;width:100%;"></div>
+		<div class="item">			<img class="img-responsive" src="assets/banner_header_<?=(isMobile())?"m_":"";?>12.png" style="object-fit:cover;width:100%;"></div>
 	</div>
 </div>
 <div class="category_title"><?=v("categories");?></div>
