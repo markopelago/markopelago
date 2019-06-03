2b0b063a7f99988d10804992ed9c9a46c48e6f49
Rigen <rigenwildanu@gmail.com>
2019-06-03 19:24:00 +0700

    index - operasional close lebaran

---
+++ index-lebaran.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="<?=$_COOKIE["locale"];?>">
+<head>
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+</head>
+
+<body>
+	<div style="text-align: center;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-lg-12 col-xs-12">
+					<img class="img-responsive" src="assets/markopelago_logo_new.jpeg" style="height:300px;width: 300px;margin: 0 auto;">
+					<h1>Mohon maaf untuk peningkatan layanan maka kami sementara offline dari tanggal 4 - 10 Jun 2019]
+					</h1>
+					<a href="https://play.google.com/store/apps/details?id=com.markopelago" target="#">
+						<img class="img-responsive" src="assets/google-play-badge.png" style="margin: 0 auto;">
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
